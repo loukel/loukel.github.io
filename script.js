66f399d17b8ff9beac38e73562063448d4a76a76
loukel <louis.kelly1@hotmail.com>
2021-10-20 09:57:56 +0100

    Made site more pirate like

---
+++ script.js
@@ -0,0 +1,6 @@
+const headingEl = document.getElementById('heading')
+const hexCodes = ['#1c8bc1', '#ddd417', '#760a0a', '#5e2a0e']
+
+setInterval(() => {
+  headingEl.style.color = hexCodes[Math.floor(Math.random() * hexCodes.length)]
+}, 100)
